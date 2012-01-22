dee249930d9a5080eff64079e973414130a62ba9
Dmitry Frank <dimon.frank@gmail.com>
2012-01-22 21:06:29 +0400

    indexer: examples changed a bit

--- doc/examples/vimprj_indexer_files/main.c
+++ doc/examples/vimprj_indexer_files/main.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 long my_global_long;
--- doc/examples/vimprj_indexer_files/main.h
+++ doc/examples/vimprj_indexer_files/main.h
@@ -1,5 +1,5 @@
 
-#include "some_folder_1/some_header.h"
+#include "some_folder_1/some_file.h"
 
 extern long my_global_long;
 extern int  my_global_int;
